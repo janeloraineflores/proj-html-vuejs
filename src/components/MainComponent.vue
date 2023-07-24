7de7fb357b2f352b0c4a667ba82812441afd2f1d
janeloraineflores <janeloraineaflores@yahoo.com>
2023-07-24 16:19:21 +0200

    Aggiunto second section main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -215,6 +215,104 @@
                 </div>
             </div>
         </section>
+
+        <!-- second section main-->
+        <section class="second-main-section py-5">
+            <div class="my-container">
+                <div class="row">
+                    <div class="col text-center">
+                        <p class="text-uppercase text-secondary">
+                            Maxcouch available courses
+                        </p>
+                        <h3 class="fw-bold">
+                            Access
+                            <span>
+                                Smart Tutoring
+                            </span>
+                            Program For Benefits.
+                        </h3>
+                    </div>
+                </div>
+
+                <div class="row mt-5">
+                    <div class="col-4">
+                        <div>
+                            <img src="../assets/img/home-2-image-box-01.png" alt="">
+                        </div>
+                        <h6 class="fw-bold mt-4">
+                            Customize Preferences
+                        </h6>
+                        <p class="text-secondary">
+                            Learners are given the right to arrange and customize their study schedule and timetable based on preferences.
+                        </p>
+                        <a href="#" class="text-decoration-none text-secondary fw-bold">
+                            Start here <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-4 px-5">
+                        <div>
+                            <img src="../assets/img/about-us-02-image-box-02.png" alt="">
+                        </div>
+                        <h6 class="fw-bold mt-4">
+                            Cloud Storage
+                        </h6>
+                        <p class="text-secondary">
+                           You can store online lessons via online cloud or download to your device and revise lessons.
+                        </p>
+                        <a href="#" class="text-decoration-none text-secondary fw-bold">
+                            Get Free Quote  <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+
+                    <div class="col-4">
+                        <div>
+                            <img src="../assets/img/home-2-image-box-03.png" alt="">
+                        </div>
+                        <h6 class="fw-bold mt-4">
+                            Multimedia Channels
+                        </h6>
+                        <p class="text-secondary">
+                           We provide study materials with various formats: video, audio, slides, doc, prints, books and applications.
+                        </p>
+                        <a href="#" class="text-decoration-none text-secondary fw-bold">
+                            Start now <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row mt-5 pt-5">
+                    <div class="col-7">
+                        <div class="second-image-container">
+                            <img src="../assets/img/home-2-popup-video-poster.jpg" alt="">
+                        </div>
+                    </div>
+
+                    <div class="col-5">
+                        <p class="text-uppercase text-secondary">
+                            Get started
+                            <span class="text-dark">
+                                Effortlessly
+                            </span>
+                        </p>
+                        <h2 class="fw-bold">
+                            Enjoy Our
+                            <br>
+                            <span class="fw-normal">
+                                Companionship
+                            </span>
+                        </h2>
+                        <p class="text-secondary mb-5">
+                            Sneek peek into what has in store for you on outr online teaching and mentoring portal. Seek for some hidden hints of early-bird rewards as well.
+                        </p>
+                        <a href="#" class="a fw-bold">
+                            Downloand free guidebook <i class="bi bi-arrow-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
     </main>
 </template>
 
@@ -252,5 +350,39 @@
             border: none
         }
 
+    }
+
+
+    .second-main-section {
+        h3 {
+            color: #3F3A64;
+        }
+
+        span {
+            color:#20ad96
+        }
+
+        h6 {
+            
+            color: #3F3A64;
+        }
+
+        h2 {
+            color: #3F3A64;
+        }
+
+        .second-image-container {
+            width: 450px;
+            height: 300px;
+
+            img {
+                width: 100%;
+                height: 100%;
+            }
+        }
+
+        a {
+            color: #20ad96;
+        }
     }
 </style>
